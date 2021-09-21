df394b79731bed913a12a68490bd809b7c2a1d75
Shradha1824 <shradhabj1824@gmail.com>
2021-09-21 14:03:16 +0530

    Check Employee Present Or Absent

--- bin/employee_wage_computation.dart
+++ bin/employee_wage_computation.dart
@@ -1,3 +1,18 @@
+import 'dart:math';
+
 void main(List<String> args) {
   print("Welcome to Employee Wage Computation Program");
+  checkEmployeeprasentorabsent();
+}
+
+checkEmployeeprasentorabsent() {
+  var r = Random();
+  var empCheck = r.nextInt(2);
+  print(empCheck);
+
+  if (empCheck == 1) {
+    print("Employee is Present");
+  } else {
+    print("Employee is Absent");
+  }
 }
