08c4d7208426a2756a8cda2bbd411dad0aa9ca5c
Shradha1824 <shradhabj1824@gmail.com>
2021-09-22 02:08:12 +0530

    TotalEmpWage

--- bin/employee_wage_computation.dart
+++ bin/employee_wage_computation.dart
@@ -1,11 +1,13 @@
 import 'dart:math';
 
 void main(List<String> args) {
+  final int num_of_working_days = 2;
   var empHrs = 0;
   var empWage = 0;
   var is_full_day = 1;
   var is_half_day = 2;
   var emp_rate_per_hr = 20;
+  int totalEmpWage = 0;
 
   var r = Random();
   var empCheck = r.nextInt(2);
@@ -56,4 +58,21 @@ void main(List<String> args) {
   }
   empWage = empHrs * emp_rate_per_hr;
   print(empWage);
+
+  for (int day = 0; day < num_of_working_days; day++) {
+    switch (empCheckForpt) {
+      case 1:
+        empHrs = 8;
+        break;
+      case 2:
+        empHrs = 4;
+        break;
+
+      default:
+        empHrs = 0;
+    }
+    empWage = empHrs * emp_rate_per_hr;
+    totalEmpWage = totalEmpWage + empWage;
+  }
+  print(totalEmpWage);
 }
