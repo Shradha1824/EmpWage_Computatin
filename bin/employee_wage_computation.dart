0bf97b1e0e5dd2bb940309af5529b210362ef4bd
Shradha1824 <shradhabj1824@gmail.com>
2021-09-21 15:07:43 +0530

    Calculating PartTime Employee Wage

--- bin/employee_wage_computation.dart
+++ bin/employee_wage_computation.dart
@@ -4,6 +4,7 @@ void main(List<String> args) {
   print('Welcome to Employee Wage Computation Program');
   checkEmployeeprasentorabsent();
   checkDailyEmpWage();
+  checkPartTimeEmpWage();
 }
 
 // ignore: always_declare_return_types
@@ -32,6 +33,31 @@ checkDailyEmpWage() {
   if (empCheck == is_full_day) {
     empHrs = 8;
     print('Employee Wage For full day');
+  } else {
+    empHrs = 0;
+    print('Employee Wage for zero day');
+  }
+
+  empWage = empHrs * emp_rate_per_hr;
+  print(empWage);
+}
+
+checkPartTimeEmpWage() {
+  var empHrs = 0;
+  var empWage = 0;
+  var is_full_day = 1;
+  var is_half_day = 2;
+  var emp_rate_per_hr = 20;
+
+  var r = Random();
+  var empCheck = r.nextInt(3);
+
+  if (empCheck == is_full_day) {
+    empHrs = 8;
+    print('Employee Wage For full day');
+  } else if (empCheck == is_half_day) {
+    empHrs = 4;
+    print('Employee Wage for Half Day');
   } else {
     empHrs = 0;
     print('Employee Wage for zero day');
