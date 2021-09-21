0b4aafdefdc73c023c4a0efac458be9312a883d4
Shradha1824 <shradhabj1824@gmail.com>
2021-09-21 15:29:51 +0530

    Code Reuse

--- bin/employee_wage_computation.dart
+++ bin/employee_wage_computation.dart
@@ -1,10 +1,48 @@
 import 'dart:math';
 
 void main(List<String> args) {
+  var empHrs = 0;
+  var empWage = 0;
+  var is_full_day = 1;
+  var is_half_day = 2;
+  var emp_rate_per_hr = 20;
+
+  var r = Random();
+  var empCheck = r.nextInt(2);
+  var empCheckForpt = r.nextInt(3);
+
   print('Welcome to Employee Wage Computation Program');
-  checkEmployeeprasentorabsent();
-  checkDailyEmpWage();
-  checkPartTimeEmpWage();
+
+  if (empCheck == 1) {
+    print('Employee is Present');
+  } else {
+    print('Employee is Absent');
+  }
+
+  if (empCheck == is_full_day) {
+    empHrs = 8;
+    print('Employee Wage For full day');
+  } else {
+    empHrs = 0;
+    print('Employee Wage for zero day');
+  }
+
+  empWage = empHrs * emp_rate_per_hr;
+  print(empWage);
+
+  if (empCheckForpt == is_full_day) {
+    empHrs = 8;
+    print('Employee Wage For full day');
+  } else if (empCheckForpt == is_half_day) {
+    empHrs = 4;
+    print('Employee Wage for Half Day');
+  } else {
+    empHrs = 0;
+    print('Employee Wage for zero day');
+  }
+
+  empWage = empHrs * emp_rate_per_hr;
+  print(empWage);
 }
 
 // ignore: always_declare_return_types
@@ -18,51 +56,4 @@ checkEmployeeprasentorabsent() {
   } else {
     print('Employee is Absent');
   }
-}
-
-// ignore: always_declare_return_types
-checkDailyEmpWage() {
-  var empHrs = 0;
-  var empWage = 0;
-  var is_full_day = 1;
-  var emp_rate_per_hr = 20;
-
-  var r = Random();
-  var empCheck = r.nextInt(2);
-
-  if (empCheck == is_full_day) {
-    empHrs = 8;
-    print('Employee Wage For full day');
-  } else {
-    empHrs = 0;
-    print('Employee Wage for zero day');
-  }
-
-  empWage = empHrs * emp_rate_per_hr;
-  print(empWage);
-}
-
-checkPartTimeEmpWage() {
-  var empHrs = 0;
-  var empWage = 0;
-  var is_full_day = 1;
-  var is_half_day = 2;
-  var emp_rate_per_hr = 20;
-
-  var r = Random();
-  var empCheck = r.nextInt(3);
-
-  if (empCheck == is_full_day) {
-    empHrs = 8;
-    print('Employee Wage For full day');
-  } else if (empCheck == is_half_day) {
-    empHrs = 4;
-    print('Employee Wage for Half Day');
-  } else {
-    empHrs = 0;
-    print('Employee Wage for zero day');
-  }
-
-  empWage = empHrs * emp_rate_per_hr;
-  print(empWage);
 }
